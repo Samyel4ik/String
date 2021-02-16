0e4ddf8e8a3c47fda3aba97b9966dfe1484e078b
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-16 22:35:19 +0300

    string

---
+++ src/Task1/Strings1.java
@@ -0,0 +1,21 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Strings1 {
+    // дан символ, если он в нижнем регистре - преобразовать в верхний,
+    // если в верхнем - то в нижний, т.е. "с" -> "С", "С" -> "с"
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите символ:");
+        String s = scanner.nextLine();
+
+        if (s == s.toUpperCase()){
+            System.out.println("Символ был в верхнем регистре");
+            System.out.println(s.toLowerCase());
+        }else {
+            System.out.println("Символ был в нижнем регистре");
+            System.out.println(s.toUpperCase());
+        }
+    }
+}
---
+++ src/Task2/Strings2.java
@@ -0,0 +1,6 @@
+package Task2;
+
+public class Strings2 {
+    //дана строка "thirdOneWithMoreWords", заменить все заглавные буквы на "_", т.е. "third_ne_ith_ore_ords"
+
+}
