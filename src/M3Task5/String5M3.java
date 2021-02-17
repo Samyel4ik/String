d0ad2c7f8dda765fa65d8e2eb8020ed1c22fe41b
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-17 22:29:16 +0300

    Task5

--- src/M3Task5/String5M3.java
+++ src/M3Task5/String5M3.java
@@ -1,7 +1,14 @@
 package M3Task5;
 
-public class String5M3 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class String5M3 {
+    //Удалить в строке все лишние пробелы, то есть серии подряд идущих
+    // пробелов заменить на одиночные пробелы. Крайние пробелы в строке удалить.
+    public static void main(String[] args) {
+        String text = "the  world  12 is beautiful  45   when 42 there11 is no 3 war in 2 the world";
+
+        text =text.replaceAll("\\s+"," ");
+        System.out.println(text);
     }
 }
