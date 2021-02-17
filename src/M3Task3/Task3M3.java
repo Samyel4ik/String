ed3a7061f98bbccdacd54b8568778fdcc97e18ad
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-17 20:25:11 +0300

    Task3

---
+++ src/M3Task3/Task3M3.java
@@ -0,0 +1,21 @@
+package M3Task3;
+
+import Task0.String0;
+
+public class Task3M3 {
+    //В строке найти количество цифр.
+    public static void main(String[] args) {
+        String str = "the world 12 is beautiful 45  when 42 there is no 3 war in 2 the world";
+        char[] array = String0.arrayString(str);
+        //char [] array = str.toCharArray();
+        
+        int number = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (Character.isDigit(array[i])) {
+                number++;
+            }
+        }
+        System.out.println("Количество чисел в строке:" + number);
+    }
+}
